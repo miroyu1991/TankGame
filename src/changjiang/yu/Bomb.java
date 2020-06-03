6550e4dc66c14b6ff74e63857b67e1641548f1cc
miroyu1991 <46637440+miroyu1991@users.noreply.github.com>
2020-06-03 18:18:14 -0400

    Add bomb class divide MyPanel class from MyTankGame class

---
+++ src/changjiang/yu/Bomb.java
@@ -0,0 +1,54 @@
+package changjiang.yu;
+
+public class Bomb {
+
+    int x;
+    int y;
+    int life = 9;
+    boolean isLive = true;
+
+    public Bomb(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public boolean isLive() {
+        return isLive;
+    }
+
+    public void setLive(boolean live) {
+        isLive = live;
+    }
+
+    public void lifeDown(){
+        if(this.life > 0){
+            this.life--;
+        }else{
+            this.isLive = false;
+        }
+    }
+}
